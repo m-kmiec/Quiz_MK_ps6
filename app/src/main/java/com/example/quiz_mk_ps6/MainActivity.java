e8492b44ff1b02a7dd18d4b82f2e10ac7ea1e7f7
Michal Kmiec <michal.kmiec01@gmail.com>
2022-10-16 15:08:11 +0200

    zadanie 2

--- app/src/main/java/com/example/quiz_mk_ps6/MainActivity.java
+++ app/src/main/java/com/example/quiz_mk_ps6/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.quiz_mk_ps6;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,24 +17,91 @@ public class MainActivity extends AppCompatActivity {
     private Button trueButton;
     private Button falseButton;
     private Button nextButton;
+    private Button hintButton;
     private TextView questionTextView;
+    private static final String QUIZ_TAG = "MainActivity";
+    private static final String KEY_CURRENT_INDEX = "currentQuestionIndex";
+    public static final String KEY_EXTRA_ANSWER = "Quiz_MK_ps6.correctAnswer";
+    private static final int REQUEST_CODE_PROMPT = 0;
+    private boolean answerWasShown;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(QUIZ_TAG,"onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(QUIZ_TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(QUIZ_TAG,"onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(QUIZ_TAG,"onPause");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(QUIZ_TAG, "Wywołana została metoda: onSaveInstanceState");
+        outState.putInt(KEY_CURRENT_INDEX, currentQuestionIndex);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(QUIZ_TAG,"onResume");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) { return; }
+        if (requestCode == REQUEST_CODE_PROMPT) {
+            if (data == null) { return; }
+            answerWasShown = data.getBooleanExtra(PromptActivity.KEY_EXTRA_ANSWER_SHOWN, false);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onCreate");
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            currentQuestionIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
+        }
 
         trueButton = findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
         nextButton = findViewById(R.id.next_button);
+        hintButton = findViewById(R.id.hint_button);
         questionTextView = findViewById(R.id.question_text_view);
 
         trueButton.setOnClickListener(view -> checkAnswerCorrectness(true));
 
         falseButton.setOnClickListener(view -> checkAnswerCorrectness(false));
 
+        hintButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, PromptActivity.class);
+            boolean correctAnswer = questions[currentQuestionIndex].getCorrectAnswer();
+            intent.putExtra(KEY_EXTRA_ANSWER, correctAnswer);
+            startActivityForResult(intent, REQUEST_CODE_PROMPT);
+        });
+
         nextButton.setOnClickListener(view -> {
             currentQuestionIndex = (currentQuestionIndex + 1) % questions.length;
+            answerWasShown = false;
             setNextQuestion();
         });
 
@@ -53,10 +124,14 @@ public class MainActivity extends AppCompatActivity {
 
         int resultMessageId = 0;
 
-        if (userAnswer == correctAnswer) {
-           resultMessageId = R.string.correct_answer;
+        if (answerWasShown) {
+            resultMessageId = R.string.answer_was_shown;
         } else {
-            resultMessageId = R.string.incorrect_answer;
+            if (userAnswer == correctAnswer) {
+                resultMessageId = R.string.correct_answer;
+            } else {
+                resultMessageId = R.string.incorrect_answer;
+            }
         }
 
         Toast.makeText(this, resultMessageId, Toast.LENGTH_SHORT).show();
